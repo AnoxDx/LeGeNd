7eb7b23a2af442403bf0dae873c1f14f724e7f36
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2023-07-07 14:05:20 +0530

    Update help.py

--- StringGenBot/help.py
+++ StringGenBot/help.py
@@ -1,1 +1,17 @@
+from pyrogram.types import Message
+from telethon import TelegramClient
+from pyrogram import Client, filters
+from asyncio.exceptions import TimeoutError
+from telethon.sessions import StringSession
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
+import config
+
+
+
+ask_ques = "**ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴘ ᴏꜰ ᴛʜɪꜱ ʙᴏᴛ ʏᴏᴜ ᴠᴀɴ ʙᴜʏ ᴛᴇʟᴇɢʀᴀᴍ ɪᴅꜱ ᴀɴᴅ ᴜꜱᴇ ꜱᴍᴍ ᴘᴀɴᴇʟ ʟɪꜱᴛᴇᴅ ɪɴ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ꜱᴍᴍ ʀᴇᴀʟᴛᴇᴅ ꜱᴇʀᴠɪᴄᴇꜱ!**"
+buttons_ques = [
+    [
+        InlineKeyboardButton("ɢᴏ ʙᴀᴄᴋ", callback_data="start"),
+        InlineKeyboardButton("ꜱᴍᴍ ᴘᴀɴᴇʟ", callback_data="")
+    ]
