8099cdb1b35868a2125b86e979a3537d1fb6000e
LeGeNd <128361863+AnoxDx@users.noreply.github.com>
2023-07-07 14:17:58 +0530

    Update help.py

--- StringGenBot/help.py
+++ StringGenBot/help.py
@@ -1,17 +1,38 @@
-from pyrogram.types import Message
-from telethon import TelegramClient
 from pyrogram import Client, filters
-from asyncio.exceptions import TimeoutError
-from telethon.sessions import StringSession
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
-import config
+from config import OWNER_ID
 
 
+def filter(cmd: str):
+    return filters.private & filters.incoming & filters.command(cmd)
 
-ask_ques = "**ᴡɪᴛʜ ᴛʜᴇ ʜᴇʟᴘ ᴏꜰ ᴛʜɪꜱ ʙᴏᴛ ʏᴏᴜ ᴠᴀɴ ʙᴜʏ ᴛᴇʟᴇɢʀᴀᴍ ɪᴅꜱ ᴀɴᴅ ᴜꜱᴇ ꜱᴍᴍ ᴘᴀɴᴇʟ ʟɪꜱᴛᴇᴅ ɪɴ ʙᴜᴛᴛᴏɴ ꜰᴏʀ ꜱᴍᴍ ʀᴇᴀʟᴛᴇᴅ ꜱᴇʀᴠɪᴄᴇꜱ!**"
-buttons_ques = [
-    [
-        InlineKeyboardButton("ɢᴏ ʙᴀᴄᴋ", callback_data="start"),
-        InlineKeyboardButton("ꜱᴍᴍ ᴘᴀɴᴇʟ", url="https://realsmmpanel.online")
-    ]
+@Client.on_message(filter("start"))
+async def start(bot: Client, msg: Message):
+    me2 = (await bot.get_me()).mention
+    await bot.send_message(
+        chat_id=msg.chat.id,
+        text=f"""Hᴇʏ {msg.from_user.mention},
+
+ ᴡᴇʟᴄᴏᴍᴇ!!! ᴜꜱᴀɢᴇ ᴄᴍᴅꜱ ʟɪꜱᴛᴇᴅ ʙᴇʟʟᴏᴡ! ꜰɪʀꜱᴛ ᴏꜰ ᴀʟʟ ᴊᴏɪɴ ᴏᴜʀ ꜱᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ ᴀɴᴅ ᴄʜᴀɴɴᴇʟ ʟɪꜱᴛᴇᴅ ʙᴇʟᴏᴡ! ᴛʜᴀɴᴋꜱ ꜰᴏʀ ᴠɪꜱɪᴛɪɴɢ!!!
+ Mᴀᴅᴇ ʙʏ : [//ᴋᴀʀᴛɪᴋ ʟᴇɢᴇɴᴅ//](tg://user?id={OWNER_ID}) !""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="ʙᴜʏ ᴛᴇʟᴇɢʀᴀᴍ ɪᴅꜱ", callback_data="generate")
+                ],
+                [
+                    InlineKeyboardButton("ꜱᴍᴍ ᴘᴀɴᴇʟ", url="https://realsmmpanel.online"),
+                    InlineKeyboardButton("ᴏꜰꜰɪᴄɪᴀʟ ᴄʜᴀɴɴᴇʟ", url="https://t.me/evonity")
+                ],
+                 [
+                    InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ", url="https://t.me/EvonixZone")
+                ],
+                 [
+                    InlineKeyboardButton("ᴀᴅᴅ ꜰᴜɴᴅ", callback_data="funding"),
+                    InlineKeyboardButton("ᴄᴏᴍᴍᴀɴᴅꜱ", callback_data="help")
+                ]
+            ]
+        ),
+        disable_web_page_preview=True,
+    )
